e27b8de85935f60152a12aca03464fda32d18c74
Abdulaziz <lchoriyev00@gmail.com>
2023-05-30 11:54:01 +0500

    init: startkit is done

---
+++ src/layouts/HeaderLayout/HeaderLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    <p>header</p>
+  </header>
+</template>
+
+<script>
+  export default{
+    
+  }
+</script>
---
+++ src/pages/HomePage/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home-page">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
